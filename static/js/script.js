5f35760374cda4593d7dbb010ebb28a2ff36a788
GuMi-Develop <maiklgusmanov@yandex.ru>
2025-10-18 23:44:49 +0300

    Create portfolio with new design

--- static/js/script.js
+++ static/js/script.js
@@ -1,55 +1,30 @@
-const checkbox = document.getElementById("switch");
-
-checkbox.addEventListener("change", () => {
-  document.querySelectorAll("*").forEach((el) => {
-    el.classList.toggle("dark", checkbox.checked);
-  });
-});
-
-const parallax = document.querySelector(".my-img");
-
-document.addEventListener("mousemove", (e) => {
-  const { innerWidth, innerHeight } = window;
-  const moveX = (e.clientX - innerWidth / 2) * 0.03; // чувствительность по X
-  const moveY = (e.clientY - innerHeight / 2) * 0.03; // чувствительность по Y
-
-  parallax.style.transform = `translate(calc(${moveX}px), calc(${moveY}px))`;
-});
-
-function setupCardFlip() {
-  const moreButtons = document.querySelectorAll(".stack-more");
-  const returnButtons = document.querySelectorAll(".stack-return");
-
-  // Обработчик для кнопки "Подробнее"
-  moreButtons.forEach((button) => {
-    button.addEventListener("click", function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      const card = this.closest(".stack-card");
-      card.classList.add("flipped");
-    });
-  });
-
-  // Обработчик для кнопки "Назад"
-  returnButtons.forEach((button) => {
-    button.addEventListener("click", function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      const card = this.closest(".stack-card");
-      card.classList.remove("flipped");
-    });
-  });
-
-  // Закрытие при клике вне карточки
-  document.addEventListener("click", function (e) {
-    if (!e.target.closest(".stack-card")) {
-      document.querySelectorAll(".stack-card.flipped").forEach((card) => {
-        card.classList.remove("flipped");
-      });
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  setupCardFlip();
+const swiper = new Swiper(".swiper", {
+  speed: 500,
+  spaceBetween: 47,
+  loop: true,
+  slidesPerView: 3,
+  centeredSlides: true,
+  slidesOffsetAfter: 45,
+  slidesOffsetBefore: 45,
+  keyboard: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1400: {
+      slidesPerView: 3,
+    },
+    720: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+  },
 });
